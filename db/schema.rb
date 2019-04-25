8729be5184558b9e139221b533b50a49f3bc652c
Adlan Elias <adlanelias@MacBook-Pro-Adlan.local>
2019-04-26 09:18:58 +1000

    commits

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_25_072514) do
+ActiveRecord::Schema.define(version: 2019_04_25_231211) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,9 @@ ActiveRecord::Schema.define(version: 2019_04_25_072514) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "club_id"
+  end
+
+  create_table "players_users", force: :cascade do |t|
   end
 
   create_table "squads", force: :cascade do |t|
